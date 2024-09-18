47c979be4eb223604475386b4663e31f6357ad0c
Peh Wei Qi (wpeh0003) <pehweiqi02@gmail.com>
2024-09-19 01:16:58 +0800

    Added company interfaces and api call script

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// console.log(searchCompanies("tsla"));
 root.render(
   <React.StrictMode>
     <App />
