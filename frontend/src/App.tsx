146610587d53c9954f8ec69327adf97189df0e88
Peh Wei Qi (wpeh0003) <pehweiqi02@gmail.com>
2024-09-20 00:37:43 +0800

    Add return type check for api calls in App.tsx and conditional rendering

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { CompanySearch } from './company';
+import { searchCompanies } from './api';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClick = async (e:SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    if (typeof result === "string"){
+      setServerError(result);
+    }else if (Array.isArray(result.data)){
+      setSearchResult(result.data);
+      console.log(result.data);
+    }
+    // console.log(searchResult);
+    // console.log(result.data);
+  };
+  // {condition && expression} is a shorthand for if (condition) {expression}
+  // {codition ? true : false} is a ternary operator
   return (
     <div className="App">
-      <Search/>
+      <Search onClick={onClick} search={search} handleChange={handleChange}/>
+      {serverError && <h1>{serverError}</h1>}
       <CardList/>
     </div>
   );
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,12 @@
 import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from "react";
 
-type Props = {};
+interface Props {
+  onClick: (e:SyntheticEvent) => void;
+  search: string | undefined; // because search is a state var, so it may be undefined
+  handleChange: (e:ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-  const [search, setSearch] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-  const onClick = (e:SyntheticEvent) => {
-    console.log(e)
-  }
+const Search: React.FC<Props> = ({onClick, handleChange, search}: Props): JSX.Element => {
 
   return (
     <div>
