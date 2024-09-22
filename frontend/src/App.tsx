2436346e98c969a3b4505122c4c370bd14b238ea
Peh Wei Qi (wpeh0003) <pehweiqi02@gmail.com>
2024-09-22 18:15:01 +0800

    Add Hero and Navbar component and change all css styling to tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import Search from "./Components/Search/Search";
 import { CompanySearch } from "./company";
 import { searchCompanies } from "./api";
 import ListPortfolio from "./Components/ListPortfolio/ListPortfolio";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 function App() {
   const [search, setSearch] = useState<string>("");
@@ -48,6 +50,7 @@ function App() {
   // {codition ? true : false} is a ternary operator
   return (
     <div className="App">
+      <Navbar/>
       <Search
         onSearchSubmit={onSearchSubmit}
         search={search}
--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,27 +1,34 @@
-import React, { SyntheticEvent } from 'react'
-import Card from '../Card/Card'
-import { CompanySearch } from '../../company'
-import { v4 as uuidv4} from 'uuid';
+import React, { SyntheticEvent } from "react";
+import Card from "../Card/Card";
+import { CompanySearch } from "../../company";
+import { v4 as uuidv4 } from "uuid";
 
 interface Props {
   searchResult: CompanySearch[];
-  onPortfolioCreate: (e:SyntheticEvent) => void;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const CardList = ({searchResult, onPortfolioCreate}: Props) => {
+const CardList = ({ searchResult, onPortfolioCreate }: Props) => {
   return (
     <div>
-      {
-        searchResult.length > 0 ? (
-          searchResult.map((result) => {
-            return <Card id = {result.symbol} key = {uuidv4()} searchResult={result} onPortfolioCreate={onPortfolioCreate}/>
-          })
-        ) : (
-          <h1>No results</h1>
-        )
-      }
+      {searchResult.length > 0 ? (
+        searchResult.map((result) => {
+          return (
+            <Card
+              id={result.symbol}
+              key={uuidv4()}
+              searchResult={result}
+              onPortfolioCreate={onPortfolioCreate}
+            />
+          );
+        })
+      ) : (
+        <p className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+          No results!
+        </p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
